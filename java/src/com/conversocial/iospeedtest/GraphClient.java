a538da80810bb180869a2b8129fcca5f2c8d7e37
Gareth Lloyd <glloyd@gmail.com>
2012-08-21 12:04:51 +0100

    single threaded

--- java/src/com/conversocial/iospeedtest/GraphClient.java
+++ java/src/com/conversocial/iospeedtest/GraphClient.java
@@ -38,7 +38,7 @@ public class GraphClient {
 		this.token = token;
 		
 		IOReactorConfig config = new IOReactorConfig();
-		config.setIoThreadCount(4);
+		config.setIoThreadCount(1);
 		config.setShutdownGracePeriod(3000);
 		config.setConnectTimeout(1000);
 		config.setSoTimeout(1000);
